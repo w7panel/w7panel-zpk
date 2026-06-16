a05ed59897ed57c37afdb72957254bcb192d87c2
rxw <1120309488@qq.com>
2026-06-16 10:51:17 +0800

    更新 helm 传统应用安装ssl 参数可能为空的问题

--- app/respo/logic/helm_pack.go
+++ app/respo/logic/helm_pack.go
@@ -1674,7 +1674,7 @@ spec:
           command:
             - sh
             - -c
-            - /home/rangine create:site --operation={{ ternary "upgrade" "install" .Release.IsUpgrade }} --w7panel-domain={{ .Values.global.panel.innerUrl }} --w7panel-token={{ .Values.global.panel.panelRealToken }} --title=%s --name=%s --language=%s --version=%s --domain={{ .Values.DOMAIN_URL }} --ssl={{ .Values.ingressForceHttps }} --cmd=%s --code-download-url=%s --app_name=%s -f /home/config.yaml
+            - /home/rangine create:site --operation={{ ternary "upgrade" "install" .Release.IsUpgrade }} --w7panel-domain={{ .Values.global.panel.innerUrl }} --w7panel-token={{ .Values.global.panel.panelRealToken }} --title=%s --name=%s --language=%s --version=%s --domain={{ .Values.DOMAIN_URL }} --ssl={{ default false .Values.ingressForceHttps }} --cmd=%s --code-download-url=%s --app_name=%s -f /home/config.yaml
 `, application.Identifie+"-"+tradition.EnvironmentVersion+"-副本", tradition.EnvironmentName, tradition.EnvironmentLanguage, tradition.EnvironmentVersion, cmd, zipUrl, application.Identifie)
 
 	filePath := filepath.Join(rootDir, "create-site-job.yaml")
